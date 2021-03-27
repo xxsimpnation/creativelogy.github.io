64a84dfc5c150bf1b92a38e7ea5a20490214150b
creativelogy <72054980+creativelogy@users.noreply.github.com>
2021-03-27 10:25:02 +0700

    First Upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let suggestions = [
+    "Kerjasan, Kudus, Jawa Tengah",
+    "Masjid Menara, Tasik, Jawa Tengah",
+    "Candi Jieun, Kudus, Jawa Tengah",
+    "Bukit Puteran, Kudus, Jawa Tengah",
+    "Rejenu, Kudus, Jawa Tengah"
+];
